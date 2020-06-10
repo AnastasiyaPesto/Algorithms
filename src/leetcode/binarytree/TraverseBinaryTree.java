0f3d7a8d9c19ab677b4c3d70f9153df57f287b71
Anastasiya Pestovnikova <pestovnikova2013@gmail.com>
2020-06-10 13:02:19 +0300

    Rename TraverseTree -> TraverseBinaryTree

--- src/leetcode/binarytree/TraverseBinaryTree.java
+++ src/leetcode/binarytree/TraverseBinaryTree.java
@@ -3,7 +3,7 @@ package leetcode.binarytree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TraverseTree {
+public class TraverseBinaryTree {
 
   public static List<Integer> preorderTraversalRecursive(TreeNode root) {
     List<Integer> res = new ArrayList<>();
@@ -12,6 +12,19 @@ public class TraverseTree {
     res.add(root.getVal());
     res.addAll(preorderTraversalRecursive(root.getLeft()));
     res.addAll(preorderTraversalRecursive(root.getRight()));
+    return res;
+  }
+
+  public static List<Integer> preorderTraversalIterative (TreeNode root) {
+    if (root == null)
+      return null;
+    List<Integer> res = new ArrayList<>();
+    TreeNode left = root.getLeft();
+    TreeNode right = root.getRight();
+    while (left != null) {
+      res.add(left.getVal());
+      left = left.getLeft();
+    }
     return res;
   }
 
--- src/leetcode/binarytree/TraverseBinaryTreeTest.java
+++ src/leetcode/binarytree/TraverseBinaryTreeTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-class TraverseTreeTest {
+class TraverseBinaryTreeTest {
 
   @Test
   public void testPreorderTraversalRecursive() {
     TreeNode nodeRRL = new TreeNode(3, null, null);
     TreeNode nodeRR = new TreeNode(2, nodeRRL, null);
     TreeNode nodeR = new TreeNode(1, null, nodeRR);
-    List<Integer> vals = TraverseTree.preorderTraversalRecursive(nodeR);
+    List<Integer> vals = TraverseBinaryTree.preorderTraversalRecursive(nodeR);
     Assertions.assertArrayEquals(new Integer[]{1, 2, 3}, vals.toArray());
   }
 
